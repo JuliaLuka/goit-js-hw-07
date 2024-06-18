843faac13adc55d5b5cc1e874f5a904c1da1373c
JuliaLuka <lukashova.yuliia26@gmail.com>
2024-06-18 22:31:12 +0200

    task-2

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,9 @@
 const categoriesList = document.querySelectorAll(".item");
-console.log(categoriesList);
+console.log(`Number of categories: ${categoriesList.length}`);
+
+categoriesList.forEach((category) => {
+  const title = category.querySelector("h2").textContent;
+  const itemElements = category.querySelectorAll("ul>li").length;
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${itemElements}`);
+});
